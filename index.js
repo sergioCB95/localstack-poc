9448942b8713fa10cd84d88cdc9fccbbda595df0
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2021-03-23 12:39:09 +0100

    chore(master): basic s3 module

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const { S3Client, AbortMultipartUploadCommand } = require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 
 module.exports = () => {
-    const start = async () => {}
+  const start = async ({ config }) => {
+    const { core: coreConfig, bucketName } = config;
+    const client = new S3Client(coreConfig);
 
-    return {start};
-}
+    const getObject = (key) =>
+      client.send(
+        new GetObjectCommand({
+          Bucket: bucketName,
+          Key: key,
+        })
+      );
 
+    return {
+      getObject,
+    };
+  };
 
+  return { start };
+};
