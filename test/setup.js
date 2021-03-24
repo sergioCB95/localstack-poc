e0177a5fb3b529dbaacea35700103cd830f1175f
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2021-03-24 09:10:37 +0100

    test(master): jest setup script

--- test/setup.js
+++ test/setup.js
@@ -1,17 +1,8 @@
-const system = require('../system');
+const waitOn = require('wait-on');
 
 module.exports = async () => {
-	const workers = parseInt(process.env.JEST_WORKERS || '1', 10);
-
-	const sys = system();
-	const { pg } = await sys.start();
-
-	for (let i = 0; i < workers; i += 1) {
-		const workerTestDb = `test_${i}`;
-		// eslint-disable-next-line no-await-in-loop
-		await pg.query(`DROP DATABASE IF EXISTS ${workerTestDb};`);
-		// eslint-disable-next-line no-await-in-loop
-		await pg.query(`CREATE DATABASE ${workerTestDb} TEMPLATE postgres;`);
-	}
-	await sys.stop();
+  await waitOn({
+    resources: ['http://localhost:4566/health'],
+    timeout: '10000',
+  });
 };
