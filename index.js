a132604ff790ecb69ac70038e667c8b9370571a7
Sergio Cámara Braña <sergio.camara@guidesmiths.com>
2021-03-24 09:09:22 +0100

    feat(master): listBuckets method

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { S3Client, ListBucketsCommand } = require('@aws-sdk/client-s3');
 
 module.exports = () => {
   const start = async ({ config }) => {
-    const { core: coreConfig, bucketName } = config;
-    const client = new S3Client(coreConfig);
+    const client = new S3Client(config);
 
-    const getObject = (key) =>
-      client.send(
-        new GetObjectCommand({
-          Bucket: bucketName,
-          Key: key,
-        })
-      );
+    const listBucket = () => client.send(new ListBucketsCommand({}));
 
     return {
-      getObject,
+      client: client,
+      listBucket,
     };
   };
 
